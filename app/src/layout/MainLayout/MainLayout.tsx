ad155078fc0293434baced94a18a581b5000dc14
Manuel Sanchis <manuelsanchistormo@gmail.com>
2026-09-03 19:23:47 +0200

    feat: add MainLayout styles

--- app/src/layout/MainLayout/MainLayout.tsx
+++ app/src/layout/MainLayout/MainLayout.tsx
@@ -2,13 +2,14 @@ import { Outlet } from 'react-router-dom';
 
 import { Footer } from '../Footer/Footer';
 import { Header } from '../Header/Header';
+import styles from './MainLayout.module.css';
 
 export const MainLayout = () => {
   return (
-    <div>
+    <div className={styles.layout}>
       <Header />
 
-      <main>
+      <main className={styles.main}>
         <Outlet />
       </main>
 
