fc2708baa6c6042d4cbd4ccf174e76e30b742e38
Manuel Sanchis <manuelsanchistormo@gmail.com>
2026-09-03 18:16:04 +0200

    feat: add HomePage

---
+++ app/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,19 @@
+export const HomePage = () => {
+  return (
+    <div>
+      <header>
+        <h1>
+          <span>Raiders</span>
+          <span>of the</span>
+          <span>Sunken City</span>
+        </h1>
+
+        <p>Hand-sculpted miniatures</p>
+      </header>
+
+      {/* <MiniatureCarousel /> */}
+
+      {/* <KickstarterBanner /> */}
+    </div>
+  );
+};
